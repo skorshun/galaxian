44972aeeaacae568dd101828c2b5737be1897228
Sergey Korshun <sergey.korshun@proton.me>
2026-01-27 18:52:49 +0200

    add background color

--- galaxian.py
+++ galaxian.py
@@ -3,16 +3,18 @@ import pygame
 
 WIDTH, HEIGHT = 1200, 800
 FPS = 60
+BACKGROUND_COLOR = (230, 230, 230)
 
 class Galaxian:
     """Class for managing game resources and behavior"""
 
-    def __init__(self, width: int, height: int, fps: int):
+    def __init__(self, width: int, height: int, fps: int, background_color: tuple[int, int, int]):
         pygame.init()
 
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((width, height))
         self.fps = fps
+        self.background_color = background_color
 
         pygame.display.set_caption("Galaxian")
 
@@ -27,9 +29,11 @@ class Galaxian:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.background_color)
+
             # Displaying the last screen drawn
             pygame.display.flip()
 
 if __name__ == "__main__":
-    ai = Galaxian(WIDTH, HEIGHT, FPS)
+    ai = Galaxian(WIDTH, HEIGHT, FPS, BACKGROUND_COLOR)
     ai.run_game()
