d4102b62d154759748c502d4a698b8afca654b75
Sergey Korshun <sergey.korshun@proton.me>
2026-02-02 19:48:09 +0200

    refactor: add docstrings

--- galaxian.py
+++ galaxian.py
@@ -113,14 +113,14 @@ class Galaxian:
             current_x += 2 * alien_width
 
     def _check_fleet_edges(self):
-        """comment here"""
+        """Reacts when the visitor reaches the edge of the screen."""
         for alien in self.aliens.sprites():
             if alien.check_edges():
                 self._change_fleet_direction()
                 break
 
     def _change_fleet_direction(self):
-        """comment here"""
+        """It lowers the entire fleet and changes its direction."""
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
